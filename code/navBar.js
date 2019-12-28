fbf384fccba4c992b153b8981937f87090d0c000
Amanda Truong <amanda.t.truong@gmail.com>
2019-12-28 13:07:13 -0500

    Add files via upload

---
+++ code/navBar.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+
+	var homebtn = document.getElementById("homeb");
+	var aboutbtn = document.getElementById("aboutb");
+	var charbtn = document.getElementById("characterb");
+	var downloadbtn = document.getElementById("downloadb");
+
+	homebtn.addEventListener("click", function(){
+		window.location='C:/Users/Hachimeroppi/Desktop/Project_Caelium/code/home.html';
+	});
+	aboutbtn.addEventListener("click", function(){
+		window.location='C:/Users/Hachimeroppi/Desktop/Project_Caelium/code/about.html';
+	});
+	charbtn.addEventListener("click", function(){
+		window.location='C:/Users/Hachimeroppi/Desktop/Project_Caelium/code/characters.html';
+	});
+	downloadbtn.addEventListener("click", function(){
+		window.location='C:/Users/Hachimeroppi/Desktop/Project_Caelium/code/download.html';
+	});
+
+}
